68979818db8867c46bdd33b7ed46946364ee1b77
Jörg Prante <joergprante@gmail.com>
2015-02-02 19:13:40 +0100

    fixing cyclic lookups in de-lemma-utf8.txt

---
+++ src/test/java/org/xbib/elasticsearch/index/analysis/baseform/DictionaryTests.java
@@ -0,0 +1,42 @@
+package org.xbib.elasticsearch.index.analysis.baseform;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.CharacterCodingException;
+
+public class DictionaryTests extends Assert {
+
+    @Test
+    public void verifyDE() throws IOException {
+        Dictionary dictionary = new Dictionary();
+        InputStreamReader reader = new InputStreamReader(getClass().getResource("/baseform/de-lemma-utf8.txt").openStream(), "UTF-8");
+        dictionary.load(reader);
+        reader.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResource("/baseform/de-lemma-utf8.txt").openStream(), "UTF-8"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (!line.startsWith("#")) {
+                if (!check(line, dictionary)) {
+                    break;
+                }
+            }
+        }
+        br.close();
+    }
+
+    private boolean check(String line, Dictionary dictionary) throws CharacterCodingException {
+        int pos = line.indexOf("\t");
+        String word = pos > 0 ? line.substring(0, pos) : line;
+        try {
+            String baseform = dictionary.lookup(word);
+        } catch (StackOverflowError e) {
+            System.err.println(line);
+            return false;
+        }
+        return true;
+    }
+}
