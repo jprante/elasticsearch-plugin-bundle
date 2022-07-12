5fc4e6e8afc4103e0ed1a48aa1cce2b69e4db937
Swen Thümmler <st@jpc.de>
2022-07-12 19:50:20 +0200

    changes for opensearch 2.1.0

--- src/test/java/org/xbib/opensearch/plugin/bundle/test/index/analysis/icu/IcuFoldingFilterTests.java
+++ src/test/java/org/xbib/opensearch/plugin/bundle/test/index/analysis/icu/IcuFoldingFilterTests.java
@@ -14,6 +14,7 @@ import java.io.StringReader;
 /**
  * ICU folding filter tests.
  */
+@SuppressWarnings("InvisibleCharacter")
 public class IcuFoldingFilterTests extends OpenSearchTokenStreamTestCase {
 
     public void testFoldingCharFilter() throws Exception {
